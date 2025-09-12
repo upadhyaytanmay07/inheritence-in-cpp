94bfcdfb751ab14c4e0d5f1a47d7d5b66f234480
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-09-12 22:20:37 +0530

    Create inhertience.cpp

---
+++ inhertience.cpp
@@ -0,0 +1,36 @@
+/* Tanmay Upadhyay
+PRN 24070123120
+ENTC B2 */
+
+#include <iostream>
+using namespace std;
+class Animal 
+{
+//base class
+public:
+    void eat() 
+    {
+        cout<<"This animal eats food."<< endl;
+    }
+};
+//Derived class
+class Dog : public Animal 
+{
+public:
+    void bark() 
+    {
+        cout << "The dog barks." << endl;
+    }
+};
+int main() 
+{
+    Dog myDog;
+    myDog.eat();   
+    myDog.bark();  
+    return 0;
+}
+/*
+Output:
+This animal eats food.
+The dog barks.
+*/
