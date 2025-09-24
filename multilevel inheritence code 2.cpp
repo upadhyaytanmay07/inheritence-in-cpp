c9096162d1027b06e77e82c12ec3bc96d9be900a
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-09-24 09:13:50 +0530

    Update multilevel inheritence code 2.cpp

--- multilevel inheritence code 2.cpp
+++ multilevel inheritence code 2.cpp
@@ -1,1 +1,43 @@
+/* Tanmay Upadhyay
+PRN 24070123120
+ENTC B2 */
 
+#include<iostream>
+using namespace std;
+
+class University {
+public:
+    string uni = "Symbiosis";
+    void showUni() {
+        cout << "University: " << uni << endl;
+    }
+};
+
+class Facility {
+public:
+    string lab = "VLSI Lab";
+    void showFacility() {
+        cout << "Facility: " << lab << endl;
+    }
+};
+
+class Department : public University, public Facility {
+public:
+    string dept = "Electronics & Communication";
+    void showDept() {
+        cout << "Department: " << dept << endl;
+    }
+};
+
+int main() {
+    Department ec;
+    ec.showUni();
+    ec.showDept();
+    ec.showFacility();
+    return 0;
+}
+
+/* OUTPUT
+University: Symbiosis
+Department: Electronics & Communication
+Facility: VLSI Lab */
